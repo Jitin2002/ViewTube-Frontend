dd1351867bb8ab6cecd6dd84ac20943bd5f70b7c
Jitin Dhakarwal <101663044+Jitin2002@users.noreply.github.com>
2024-08-26 00:57:35 +0530

    Update axios.helper.js

--- src/helpers/axios.helper.js
+++ src/helpers/axios.helper.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 // import { loadProgressBar } from "axios-progress-bar";
 // import "axios-progress-bar/dist/nprogress.css";
-import { BASE_URL } from "../constants";
+// import { BASE_URL } from "../constants";
+const baseURL = "https://viewtube-backend-wec6.onrender.com/api/v1";
 export const axiosInstance = axios.create({
-  baseURL: BASE_URL,
+  baseURL,
   withCredentials: true, // Ensure credentials like cookies are sent
 });
 
